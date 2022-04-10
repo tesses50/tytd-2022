47e5f2131568df6445f96b4b1350d0d6f1cade5d
Michael Nolan <mnolan1202@gmail.com>
2022-04-09 19:20:59 -0500

    Version 1.0.1

--- Tesses.YouTubeDownloader.YTGET/ProgressBar.cs
+++ Tesses.YouTubeDownloader.YTGET/ProgressBar.cs
@@ -5,6 +5,7 @@ using System.Threading;
 /// <summary>
 /// An ASCII progress bar
 /// </summary>
+/// https://gist.github.com/DanielSWolf/0ab6a96899cc5377bf54
 public class ProgressBar : IDisposable, IProgress<double> {
 	private const int blockCount = 10;
 	private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
